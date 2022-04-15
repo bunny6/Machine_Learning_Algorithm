87eff762fecb6ae6546866bb7becf8e74d48ec33
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-15 17:32:12 +0530

    Update kernel_pca.py

--- kernel_pca.py
+++ kernel_pca.py
@@ -1,73 +1,45 @@
-#!/usr/bin/env python
-# coding: utf-8
+#importing the libraries
 
-# # Kernel PCA
-
-# ## Importing the libraries
-
-# In[2]:
-
-
-import numpy as np                       #importing the libraries
+import numpy as np                       
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#importing the dataset and sploting it into X and Y.
 
-# ## Importing the dataset
-
-# In[3]:
-
-
-dataset = pd.read_csv('Wine.csv')       #importing the dataset.
+dataset = pd.read_csv('Wine.csv')       
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 
-# ## Splitting the dataset into the Training set and Test set
+#Splitting the dataset into the Training set and Test set.
 
-# In[4]:
-
-
-from sklearn.model_selection import train_test_split              #spliting the data into training and testing.
+from sklearn.model_selection import train_test_split           
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 
-# ## Feature Scaling
+#Applying Feature Scaling.
 
-# In[5]:
-
-
-from sklearn.preprocessing import StandardScaler             #doing the scaling on dataset.
+from sklearn.preprocessing import StandardScaler             
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 
-# ## Applying Kernel PCA
+#importing and applying kernal. kernal is radical basis function
 
-# In[6]:
-
-
-from sklearn.decomposition import KernelPCA             #importing and applying kernal. kernal is radical basis function
+from sklearn.decomposition import KernelPCA            
 kpca = KernelPCA(n_components = 2, kernel = 'rbf')
 X_train = kpca.fit_transform(X_train)
 X_test = kpca.transform(X_test)
 
 
-# ## Training the Logistic Regression model on the Training set
+#Training the Logistic Regression model on the Training set.
 
-# In[7]:
-
-
-from sklearn.linear_model import LogisticRegression     #training model: Logistic regression. 
+from sklearn.linear_model import LogisticRegression   
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
 
-
-# ## Making the Confusion Matrix
-
-# In[8]:
-
+#Checking the performance of the model by Creating Confusion matrix and printing  accuracy score.
 
 from sklearn.metrics import confusion_matrix, accuracy_score  #accuracy of the model.
 y_pred = classifier.predict(X_test)
@@ -76,10 +48,7 @@ print(cm)
 accuracy_score(y_test, y_pred)
 
 
-# ## Visualising the Training set results
-
-# In[9]:
-
+#Visualising the Training set results.
 
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
@@ -99,10 +68,7 @@ plt.legend()
 plt.show()
 
 
-# ## Visualising the Test set results
-
-# In[10]:
-
+#Visualising the Test set results.
 
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
