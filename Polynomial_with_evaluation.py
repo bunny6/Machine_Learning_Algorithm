b62a23948e8d883dbee56cf17ea7aa65ae6e567b
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-15 16:05:18 +0530

    Update Polynomial_with_evaluation.py

--- Polynomial_with_evaluation.py
+++ Polynomial_with_evaluation.py
@@ -1,87 +1,73 @@
-#!/usr/bin/env python
-# coding: utf-8
+#importing the libraries.
 
-# In[1]:
 
+
+
+
+#training the model on training dataset.
+#predicting on test dataset.
+#Printing the actual and the predicted data side by side.
+#Checking the performance of the model by Creating Confusion matrix and printing  accuracy score.
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# In[4]:
-
+#importing the dataset.
 
 df=pd.read_csv('Data1.csv')
 X=df.iloc[:,:-1].values
 Y=df.iloc[:,-1].values
 
-
-# In[5]:
-
-
 df.head()
 
-
-# In[8]:
-
+#Spliting the data in train and test.
 
 from sklearn.model_selection import train_test_split
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=1)
 
 
-# In[12]:
-
+#importing the logistic regression and polynomial regression.
 
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 
 
-# In[14]:
 
+#Defining the polynomial feature. 
 
-poly=PolynomialFeatures(degree=4)
+poly=PolynomialFeatures(degree=4)    #Giving degree value = 4. Polynomial function will have value = 4.
 X_poly=poly.fit_transform(X_train)
 lr=LinearRegression()
 
 
-# In[16]:
-
+#Predicting on training dataset.
 
 lr.fit(X_poly,Y_train)
 
 
-# In[17]:
-
+#Predicting on Test Dataset.
 
 y_pred=lr.predict(poly.transform(X_test))
 
 
-# In[29]:
-
-
+#Copying the data of y_test into df2.
 df2=Y_test.copy()
 
-
-# In[22]:
-
+#importing the performance metrics.
 
 from sklearn.metrics import r2_score
 
-
-# In[23]:
-
+#checking the performance on test dataset.
 
 r2_score(Y_test,y_pred)
-
-
-# In[30]:
 
 
 y_pred.shape
 
 
-# In[ ]:
+
 
 
 
