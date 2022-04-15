4e33e9628301003cb78aae331cdedc241c1fbe9d
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-15 15:51:06 +0530

    Update Multi_Linear_Regression.py

--- Multi_Linear_Regression.py
+++ Multi_Linear_Regression.py
@@ -1,88 +1,59 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[34]:
-
+#importing the dataset.
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
-# In[35]:
-
+#importing the dataset and spliting the data into X and Y.
 
 df=pd.read_csv("50_Startups.csv")
-
-
-# In[36]:
-
-
-
 X = df.iloc[:, :-1].values
 y = df.iloc[:, -1].values
 
-
-# In[37]:
-
+#importing ColumnTransformer and OneHotEncoding for Encoding.
 
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 
-
-# In[38]:
-
+#Encoding the data by using OneHotEncoder.
 
 ct=ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[3])],remainder='passthrough')
 X=np.array(ct.fit_transform(X))
 
-
-# In[39]:
-
+#Spliting the data into train and test.
 
 from sklearn.model_selection import train_test_split
 X_train,X_test,Y_train,Y_test=train_test_split(X,y,test_size=0.2,random_state=1)
 
 
-# In[40]:
-
+#Importing linear regression.
 
 from sklearn.linear_model import LinearRegression
-
-
-# In[41]:
-
-
 lr=LinearRegression()
 
 
-# In[42]:
-
+# Training the model on training dataset.
 
 lr.fit(X_train,Y_train)
 
-
-# In[43]:
-
+#Predicting on the training data.
 
 Y_train_pred=lr.predict(X_train)
 
-
-# In[44]:
-
+#Printing the actual and the predicted data side by side for training dataset.
 
 print(np.concatenate((Y_train_pred.reshape(len(Y_train_pred),1), Y_train.reshape(len(Y_train),1)),1))
 
-
-# In[45]:
-
+#predicting on test dataset.
 
 y_test_pred = lr.predict(X_test)
+
+##Checking the performance of the model by Creating Confusion matrix and printing  accuracy score
 np.set_printoptions(precision=2)
 print(np.concatenate((y_test_pred.reshape(len(y_test_pred),1), Y_test.reshape(len(Y_test),1)),1))
 
 
-# In[ ]:
 
 
 
