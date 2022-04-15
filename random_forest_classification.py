93a985f4e07b34b78537ce4c8c0c9a4fbd2e4518
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-15 16:51:33 +0530

    Update random_forest_classification.py

--- random_forest_classification.py
+++ random_forest_classification.py
@@ -1,41 +1,24 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Random Forest Classification
-
-# ## Importing the libraries
-
-# In[ ]:
-
+# Importing the libraries
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
-# ## Importing the dataset
+# Importing the dataset
 
-# In[ ]:
-
-
-dataset = pd.read_csv('ENTER_THE_NAME_OF_YOUR_DATASET_HERE.csv')
+dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 
-# ## Splitting the dataset into the Training set and Test set
-
-# In[ ]:
-
+# Splitting the dataset into the Training set and Test set
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 
-# ## Feature Scaling
-
-# In[ ]:
-
+# Feature Scaling
 
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
@@ -43,20 +26,13 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 
-# ## Training the Random Forest Classification model on the Training set
-
-# In[ ]:
-
+# Training the Random Forest Classification model on the Training set
 
 from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
 classifier.fit(X_train, y_train)
 
-
-# ## Making the Confusion Matrix
-
-# In[ ]:
-
+#Checking the performance of the model by Creating Confusion matrix and printing  accuracy score.
 
 from sklearn.metrics import confusion_matrix, accuracy_score
 y_pred = classifier.predict(X_test)
