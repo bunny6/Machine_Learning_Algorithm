bb1b0361fdad5acb6def8d43d33f0b9eaf367155
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-15 15:43:50 +0530

    Update and rename Decision_Tree.py to Basic_ML_Algorithms/Linear/Decision_Tree.py

--- Basic_ML_Algorithms/Linear/Decision_Tree.py
+++ Basic_ML_Algorithms/Linear/Decision_Tree.py
@@ -1,72 +1,52 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
+#importing the libraries.
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# In[5]:
-
+#importing the dataset and spliting it into X and Y.
 
 df=pd.read_csv("Data1.csv")
 X=df.iloc[:,:-1].values
 Y=df.iloc[:,-1].values
 
 
-# In[4]:
-
-
 df.head()
 
-
-# In[6]:
-
+#Spliting the data into Train and test.
 
 from sklearn.model_selection import train_test_split
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=1)
 
 
-# In[7]:
-
+#Importing Decision tree.
 
 from sklearn.tree import DecisionTreeRegressor
 
 
-# In[8]:
-
-
+#Defining the Decision Tree.
 dt=DecisionTreeRegressor(random_state=1)
 
 
-# In[9]:
-
+#Training the model on training dataset.
 
 dt.fit(X_train,Y_train)
 
 
-# In[11]:
-
+#Predicting on the test data.
 
 Y_test_pred=dt.predict(X_test)
 
 
-# In[13]:
-
+#importing the r2_score.
 
 from sklearn.metrics import r2_score
 
-
-# In[14]:
-
+#checking model performance.
 
 r2_score(Y_test,Y_test_pred)
 
-
-# In[ ]:
 
 
 
