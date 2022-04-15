705990f5bf7747db8e15270f5a01692008c764bb
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-15 17:00:05 +0530

    Update kernel_svm.py

--- kernel_svm.py
+++ kernel_svm.py
@@ -1,62 +1,38 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Kernel SVM
-
-# ## Importing the libraries
-
-# In[ ]:
-
+# Importing the libraries
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
-# ## Importing the dataset
+# Importing the dataset
 
-# In[ ]:
-
-
-dataset = pd.read_csv('ENTER_THE_NAME_OF_YOUR_DATASET_HERE.csv')
+dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 
-# ## Splitting the dataset into the Training set and Test set
-
-# In[ ]:
-
+# Splitting the dataset into the Training set and Test set
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 
-# ## Feature Scaling
-
-# In[ ]:
-
+# Applying Feature Scaling
 
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-
-# ## Training the Kernel SVM model on the Training set
-
-# In[ ]:
-
+# Training the Kernel SVM model on the Training set
 
 from sklearn.svm import SVC
 classifier = SVC(kernel = 'rbf', random_state = 0)
 classifier.fit(X_train, y_train)
 
 
-# ## Making the Confusion Matrix
-
-# In[ ]:
-
+#Checking the performance of the model by Creating Confusion matrix and printing  accuracy score.
 
 from sklearn.metrics import confusion_matrix, accuracy_score
 y_pred = classifier.predict(X_test)
