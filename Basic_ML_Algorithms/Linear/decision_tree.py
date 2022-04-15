eac400f60265ea34a786c19e7c36d1b13cede136
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-15 16:39:26 +0530

    Update and rename decision_tree_classification.py to Basic_ML_Algorithms/Linear/decision_tree.py

--- Basic_ML_Algorithms/Linear/decision_tree.py
+++ Basic_ML_Algorithms/Linear/decision_tree.py
@@ -1,41 +1,26 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# # Decision Tree Classification
 
-# ## Importing the libraries
-
-# In[ ]:
-
+# Importing the libraries
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
-# ## Importing the dataset
-
-# In[ ]:
-
+# Importing the dataset
 
 dataset = pd.read_csv('ENTER_THE_NAME_OF_YOUR_DATASET_HERE.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 
-# ## Splitting the dataset into the Training set and Test set
-
-# In[ ]:
-
+# Splitting the dataset into the Training set and Test set
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 
-# ## Feature Scaling
-
-# In[ ]:
-
+# Feature Scaling
 
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
@@ -43,23 +28,22 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 
-# ## Training the Decision Tree Classification model on the Training set
-
-# In[ ]:
+# Training the Decision Tree Classification model on the Training set
 
 
 from sklearn.tree import DecisionTreeClassifier
-classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)  #entropy is function used to measure the quality of a split.
 classifier.fit(X_train, y_train)
 
 
-# ## Making the Confusion Matrix
+#predicting on test data
 
-# In[ ]:
+y_pred = classifier.predict(X_test)
 
+#Importing confusion matrix and accuracy for checking the model performance.
 
 from sklearn.metrics import confusion_matrix, accuracy_score
-y_pred = classifier.predict(X_test)
+
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 accuracy_score(y_test, y_pred)
