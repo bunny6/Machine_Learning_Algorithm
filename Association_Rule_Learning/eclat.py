6de2643aaa5128c82ec3b0a3149e4eb3eccfd477
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-15 17:13:50 +0530

    Update eclat.py

--- Association_Rule_Learning/eclat.py
+++ Association_Rule_Learning/eclat.py
@@ -1,13 +1,10 @@
-# Eclat
-
-# Run the following command in the terminal to install the apyori package: pip install apyori
 
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Data Preprocessing
+# importing the dataset and data preprocessing 
 dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
 transactions = []
 for i in range(0, 7501):
@@ -17,13 +14,12 @@ for i in range(0, 7501):
 from apyori import apriori
 rules = apriori(transactions = transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2, max_length = 2)
 
-# Visualising the results
 
-## Displaying the first results coming directly from the output of the apriori function
+# Displaying the first results coming directly from the output of the apriori function
 results = list(rules)
 results
 
-## Putting the results well organised into a Pandas DataFrame
+# Putting the results well organised into a Pandas DataFrame
 def inspect(results):
     lhs         = [tuple(result[2][0][0])[0] for result in results]
     rhs         = [tuple(result[2][0][1])[0] for result in results]
