333e5eee70154d767ee43c9bf6986105c54771af
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-16 15:30:40 +0530

    Update Logistic_regression.py

--- Basic_ML_Algorithms/Classification/Logistic_regression.py
+++ Basic_ML_Algorithms/Classification/Logistic_regression.py
@@ -1,11 +1,9 @@
 #importing the libraries.
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 #importing the dataset.
-
 df=pd.read_csv("Social_Network_Ads.csv")
 X=df.iloc[:,:-1].values
 Y=df.iloc[:,-1].values
@@ -13,12 +11,10 @@ Y=df.iloc[:,-1].values
 df.head()
 
 #Spliting the data in train and test.
-
 from sklearn.model_selection import train_test_split
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.25,random_state=1)
 
 #Scaling the data.
-
 from sklearn.preprocessing import StandardScaler
 
 sc=StandardScaler()
@@ -26,11 +22,9 @@ X_train=sc.fit_transform(X_train)
 X_test=sc.transform(X_test)
 
 #importing the logistic regression. 
-
 from sklearn.linear_model import LogisticRegression
 
 #training the model on training dataset.
-
 lr=LogisticRegression(random_state=1)
 lr.fit(X_train,Y_train)
 
