d0ee6dca311a8bdb7d8a19e743497a8222d661cf
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-16 15:46:29 +0530

    Update linear_discriminant_analysis.py

--- Dimensionality_reduction/linear_discriminant_analysis.py
+++ Dimensionality_reduction/linear_discriminant_analysis.py
@@ -1,56 +1,42 @@
 #importing neccessary libraries
-
 import numpy as np                      
 import matplotlib.pyplot as plt
 import pandas as pd
 
 #importing dataset and spliting it.
-
 dataset = pd.read_csv('Wine.csv')       
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-
 #Splitting the dataset into the Training set and Test set.
-
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-
 #Applying Feature Scaling.
-
-
 from sklearn.preprocessing import StandardScaler         
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # Applying LDA
-
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA   
 lda = LDA(n_components = 2)
 X_train = lda.fit_transform(X_train, y_train)
 X_test = lda.transform(X_test)
 
-
 #Training the Logistic Regression model on the Training set
-
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
 
 #Checking the performance of the model by Creating Confusion matrix and printing  accuracy score.
-
 from sklearn.metrics import confusion_matrix, accuracy_score   #accuracy metrics.
 y_pred = classifier.predict(X_test)
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 accuracy_score(y_test, y_pred)
 
-
 #Visualising the Training set results
-
-
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
@@ -68,9 +54,7 @@ plt.ylabel('LD2')
 plt.legend()
 plt.show()
 
-
 #Visualising the Test set results
-
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
