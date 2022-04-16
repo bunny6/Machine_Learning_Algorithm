4d256d1abe00d9cc239b30ef1e1dec695b05b573
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-16 15:44:25 +0530

    Update Principal_component_analysis.py

--- Dimensionality_reduction/Principal_component_analysis.py
+++ Dimensionality_reduction/Principal_component_analysis.py
@@ -1,54 +1,42 @@
 #Importing the libraries
-
 import numpy as np                       
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-
 dataset = pd.read_csv('Wine.csv')                
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 #Splitting the dataset into the Training set and Test set.
-
 from sklearn.model_selection import train_test_split      
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 #Applying Feature Scaling.
-
 from sklearn.preprocessing import StandardScaler  
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-
 #Applying PCA to get lower dimentionality.
-
 from sklearn.decomposition import PCA        
 pca = PCA(n_components = 2)                  #n_components means no. of features we want to extract.
 X_train = pca.fit_transform(X_train)
 X_test = pca.transform(X_test)
 
-
 #Training the Logistic Regression model on the Training set.
-
 from sklearn.linear_model import LogisticRegression     
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
 
-
 #Checking the performance of the model by Creating Confusion matrix and printing  accuracy score.
-
 from sklearn.metrics import confusion_matrix, accuracy_score   #checking the performanve of the model.
 y_pred = classifier.predict(X_test)
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 accuracy_score(y_test, y_pred)
 
-
 #Visualising the Training set results.
-
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
@@ -66,9 +54,7 @@ plt.ylabel('PC2')
 plt.legend()
 plt.show()
 
-
 #Visualising the Test set results.
-
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
