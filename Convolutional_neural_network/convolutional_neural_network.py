9f63aaceac6b24acee8c51a088ad2a57e4328536
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-16 15:43:25 +0530

    Update convolutional_neural_network.py

--- Convolutional_neural_network/convolutional_neural_network.py
+++ Convolutional_neural_network/convolutional_neural_network.py
@@ -1,15 +1,12 @@
 #imported tensorflow
 #ImageDataGenerator is a tool that will applt transformation.
-
 import tensorflow as tf                                 
 from keras.preprocessing.image import ImageDataGenerator 
 
 #checking the version of tensorflow
-
 tf.__version__              
 
 #Preprocessing the Training set
-
 train_datagen = ImageDataGenerator(rescale = 1./255,       #rescale is used for scaling.
                                    shear_range = 0.2,      #shear_range specifies the angle of the slant in degrees.
                                    zoom_range = 0.2,       #to zoom in and zoom out.
@@ -21,8 +18,6 @@ training_set = train_datagen.flow_from_directory('dataset/training_set/',  #here
 
 
 #Preprocessing the Test set
-
-
 test_datagen = ImageDataGenerator(rescale = 1./255)
 test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             target_size = (64, 64),
@@ -30,53 +25,34 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             class_mode = 'binary')
 
 
-
-
 #Initialising the CNN
-
 cnn = tf.keras.models.Sequential()               
 
-
 #creating the convolutional layer.
-
 cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=[64, 64, 3])) 
 
-
 #Pooling layer.
-
 cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2)) #creating the pooling layer.
 
-
 #Adding a second convolutional layer
-
 cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))  
 cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
 
-
 #Flattening
-
 cnn.add(tf.keras.layers.Flatten())   #flattening means converting rows into sinle column. The required input format to the ann.
 
-
 #Full Connection
-
 cnn.add(tf.keras.layers.Dense(units=128, activation='relu'))   #initialising the ann.
 
 cnn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))  #creating the output layer. Unit 1 because we have one output neuron.
 
-
 #Training the CNN
-
 cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])  #compiling the cnn. Optimizer is stcoustic gradient descent method.
 
-
 #Training the CNN on the Training set and evaluating it on the Test set
-
 cnn.fit(x = training_set, validation_data = test_set, epochs = 25)  
 
-
 #Making a single prediction
-
 import numpy as np                  
 from keras.preprocessing import image
 test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))   #loading the image by using load_img function.
@@ -88,7 +64,6 @@ if result[0][0] == 1:
   prediction = 'dog'
 else:
   prediction = 'cat'
-
 
 #printing the result
 print(prediction)   
